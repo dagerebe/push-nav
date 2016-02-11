d128525adf222e8420cd6bf623bbda452fda0255
dagerebe <rebeccadagenais@Rebeccas-MacBook-Pro.local>
2016-02-11 13:31:07 -0500

    feat(*):existing class files added

---
+++ js/push-menu.js
@@ -0,0 +1,21 @@
+// Needs latest version of jQuery to run
+
+$(function(){
+  $('body').removeClass('noscript');
+
+  $('.toggle-btn, .close-btn').click(function(){
+    toggleNav();
+
+  });
+});
+
+function toggleNav(){
+  if($('.site-wrapper').attr('data-state') == 'slide-closed'){
+    // Show when nav closed
+    $('.site-wrapper').attr('data-state', 'slide-open');
+  }else{
+    // Hide nav when open
+    $('.site-wrapper').attr('data-state', 'slide-closed');
+  }
+
+}
